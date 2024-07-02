cdbff73680de0d5a928fe5c9cacea48af1539a50
LeaOhyah <leasehat@gmail.com>
2024-07-02 11:17:20 +0800

    menghapus komentar login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" value="{{ old('email') }}" required>
-           
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" required>
-            
-        </div>
-         
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
-</body>
-</html> -->
-
-
-
-
 @extends('layouts.main')
 
 @section('title', 'Login')
